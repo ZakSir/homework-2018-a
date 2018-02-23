5c0dd6f3bb009d5b389881ee7126263f6db07164
Zak Fargo <zfargo@microsoft.com>
2018-02-23 12:19:51 -0800

    Adding solution, HomeworkProblemA and Homework Problem A Tests

---
+++ test/unit/Homework.ProblemA.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Homework.ProblemA.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}

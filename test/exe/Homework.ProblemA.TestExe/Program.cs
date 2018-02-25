cb4fac7c1f8a3fe86ff01de9b0f940e2c71d588a
Zak <hirezakfargo@gmail.com>
2018-02-25 15:09:35 -0800

    fixing build issues

--- test/exe/Homework.ProblemA.TestExe/Program.cs
+++ test/exe/Homework.ProblemA.TestExe/Program.cs
@@ -9,15 +9,7 @@ namespace Homework.ProblemA.TestExe
     {
         static unsafe void Main(string[] args)
         {
-            bool bool_a = false;
-            bool bool_b = false;
-
-            object obj_a = bool_a;
-            object obj_b = bool_b;
-
-            int* addr_bool_a = &bool_a;
-
-            Console.WriteLine($"{nameof(bool_a)} @ {(&bool_a).ToString()} == {bool_a})
+           
         }
     }
 }
